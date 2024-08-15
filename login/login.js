b1c678ba8cf2867e245ec70862df735cf7643ea8
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-15 20:24:53 +0500

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,40 +1,28 @@
-let box = document.querySelector(".box");
-// let log = document.getElementById("log");
-// log.style.width = `700px`
-
-// box.innerHTML = `
-// `
-
-
-
-
-
-
-
-
 
 let formfield = document.querySelectorAll("form input")
 
-
-console.log(formfield)
 const [loginEmail, loginPassword] = formfield;
 
-let data = JSON.parse(localStorage.getItem("userData"))
-
-const login = () => {
-
-    event.preventDefault();
-    const { UserEmail, UserPassword } = data;
-
-    if (UserEmail === loginEmail.value && UserPassword === loginPassword.value) {
-        alert("login")
-    } else {
-        alert("invalid")
-    }
 
 
-    // console.log(loginEmail.value , loginPassword.value)
-    // console.log(data)
-    window.location.href = "../dashboard/dash.html";
 
-}
+// console.log(formfield)
+// let data = JSON.parse(localStorage.getItem("userData"))
+
+// const login = () => {
+
+//     event.preventDefault();
+//     const { UserEmail, UserPassword } = data;
+
+//     if (UserEmail === loginEmail.value && UserPassword === loginPassword.value) {
+//         alert("login")
+//     } else {
+//         alert("invalid")
+//     }
+
+
+//     // console.log(loginEmail.value , loginPassword.value)
+//     // console.log(data)
+//     window.location.href = "../dashboard/dash.html";
+
+// }

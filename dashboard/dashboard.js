989719999e627d0c080a972a9e278a714615d8bc
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-15 20:25:46 +0500

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,17 +1,30 @@
-let data = JSON.parse(localStorage.getItem("userData"))
-let Usename = document.getElementById("name")
-let images = document.getElementById("img")
 
-const showdata = () =>{
-    const {user, pic} = data
+
+
+
+
+
+
+
+
+
+
+
+
+// let data = JSON.parse(localStorage.getItem("userData"))
+// let Usename = document.getElementById("name")
+// let images = document.getElementById("img")
+
+// const showdata = () =>{
+//     const {user, pic} = data
     
-    Usename.innerHTML = user;
-    images.src = pic;
-    images.style.width = "50px"
-}
-showdata();
+//     Usename.innerHTML = user;
+//     images.src = pic;
+//     images.style.width = "50px"
+// }
+// showdata();
 
-const logout = () =>{
-    // console.log("hello")
-    window.location.href = "../signup/signup.html"
-}
+// const logout = () =>{
+//     // console.log("hello")
+//     window.location.href = "../signup/signup.html"
+// }

0daa2219ef31f915cc0e38d73a9bb98f4d07bb58
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-16 00:42:34 +0500

    Update login.js

--- login/login.js
+++ login/login.js
@@ -38,12 +38,14 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
+    //   const uid = user.uid;
+    window.location.href = "../dashboard/dashboard.html"
       // ...
-    } else {
-      // User is signed out
-      // ...
-    }
+    } 
+    // else {
+    //   // User is signed out
+    //   // ...
+    // }
   });
 
 

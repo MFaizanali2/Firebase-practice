94286f5b9d9beade72cadc86a1ba7f2a2934df60
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-09-03 00:11:21 +0500

    Update firebase.js

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -1,23 +1,23 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-// import { getAuth,GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth,GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB73Kv1cQ3pTV19I5nqReVGtxX0FprbtsQ",
-//     authDomain: "login-form-e13ad.firebaseapp.com",
-//     projectId: "login-form-e13ad",
-//     storageBucket: "login-form-e13ad.appspot.com",
-//     messagingSenderId: "410563988751",
-//     appId: "1:410563988751:web:ad59726ff38a8742db427a",
-//     measurementId: "G-NG4MQ2N0G4"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB73Kv1cQ3pTV19I5nqReVGtxX0FprbtsQ",
+    authDomain: "login-form-e13ad.firebaseapp.com",
+    projectId: "login-form-e13ad",
+    storageBucket: "login-form-e13ad.appspot.com",
+    messagingSenderId: "410563988751",
+    appId: "1:410563988751:web:ad59726ff38a8742db427a",
+    measurementId: "G-NG4MQ2N0G4"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
 
-// export {auth,GoogleAuthProvider}
+export {auth,GoogleAuthProvider}

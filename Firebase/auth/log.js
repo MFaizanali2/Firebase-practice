bc7e596a51786700b3e557165593b0aaec0c6f5f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-09-15 19:50:20 +0500

    Update log.js

--- Firebase/auth/log.js
+++ Firebase/auth/log.js
@@ -5,7 +5,7 @@ let logoutbtn = document.getElementById('logoutbtn')
 const provider = new GoogleAuthProvider();
 
 const googleSign = () => {
-    console.log("clicked")
+    console.log("signin")
     signInWithPopup(auth, provider)
         .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
@@ -26,7 +26,24 @@ const googleSign = () => {
 }
 
 const logout = () =>{
-    console.log("click")
+    console.log("logout")
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log(user)
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    console.log(errorMessage)
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 }
 
 

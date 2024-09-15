00d17623b9108533abd2559a2c5a708f9c91d643
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-09-15 20:30:19 +0500

    Update log.js

--- Firebase/auth/log.js
+++ Firebase/auth/log.js
@@ -1,40 +1,63 @@
-import { auth, GoogleAuthProvider, signInWithPopup, signOut } from '../firebase.js';
+import { auth, GoogleAuthProvider, signInWithPopup, signOut, signInWithEmailAndPassword } from '../firebase.js';
 
-let googleBtn = document.getElementById('googleBtn');
-let logoutbtn = document.getElementById('logoutbtn')
-const provider = new GoogleAuthProvider();
+// let googleBtn = document.getElementById('googleBtn');
+// let logoutbtn = document.getElementById('logoutbtn')
+// const provider = new GoogleAuthProvider();
 
-const googleSign = () => {
-    console.log("signin")
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            console.log(user) 
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
-}
+// const googleSign = () => {
+//     console.log("signin")
+//     signInWithPopup(auth, provider)
+//         .then((result) => {
+//             // This gives you a Google Access Token. You can use it to access the Google API.
+//             const credential = GoogleAuthProvider.credentialFromResult(result);
+//             const token = credential.accessToken;
+//             const user = result.user;
+//             console.log(user) 
+//             // ...
+//         }).catch((error) => {
+//             // Handle Errors here.
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             console.log(errorMessage)
+//             const email = error.customData.email;
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//             // ...
+//         });
+// }
 
-const logout = () =>{
-    console.log("logout")
-    signOut(auth).then(() => {
-        // Sign-out successful.
-      }).catch((error) => {
-        // An error happened.
-      });
+// const logout = () =>{
+//     console.log("logout")
+//     signOut(auth).then(() => {
+//         // Sign-out successful.
+//       }).catch((error) => {
+//         // An error happened.
+//       });
   
+// }
+
+
+// logoutbtn.addEventListener('click', logout)
+// googleBtn.addEventListener('click', googleSign)
+
+
+let email = document.getElementById("email");
+let password = document.getElementById("password")
+let signup = document.getElementById("signupbtn")
+
+const signInAdmin = () =>{
+    event.preventDefault();
+    console.log("admin signin")
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+  });
 }
 
-
-logoutbtn.addEventListener('click', logout)
-googleBtn.addEventListener('click', googleSign)
+signup.addEventListener('click', signInAdmin)
